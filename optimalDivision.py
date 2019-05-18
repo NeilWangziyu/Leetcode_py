29ce99a7c7cabe80784436cb98cbdceb7be06fbc
Ziyu <16078718+NeilWangziyu@users.noreply.github.com>
2019-05-18 13:24:58 +0800

    5-18

---
+++ optimalDivision.py
@@ -0,0 +1,34 @@
+class Solution:
+    def optimalDivision(self, nums) -> str:
+        def devid(list_nums):
+            if not list_nums:
+                return ""
+            if len(list_nums) == 1:
+                return str(list_nums[0])
+            if len(list_nums) == 2:
+                return str(list_nums[0]) + '/' + str(list_nums[1])
+
+            res = devid(list_nums[:-1]) + '/' + str(list_nums[-1])
+            return res
+
+
+
+        if not nums:
+            return ""
+        if len(nums) == 1:
+            return str(nums[0])
+        if len(nums) == 2:
+            return str(nums[0]) + '/' + str(nums[1])
+
+        res = ""
+        res += str(nums[0])
+        res += '/'
+
+        res += '(' + devid(nums[1:]) + ')'
+
+        return res
+
+
+
+s = Solution()
+print(s.optimalDivision(nums=[1000,100,10,2]))
