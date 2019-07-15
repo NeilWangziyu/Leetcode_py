f3d7925052c3530c8cd2d800ae7ae6adceede8c6
Ziyu Wang <17210720158@fudan.edu.cn>
2019-07-15 23:58:23 +0800

    tt

--- longestWPI.py
+++ longestWPI.py
@@ -19,10 +19,50 @@ class Solution:
 
         return ans
 
+
+    def longestWPI_DP(self, hours: List[int]) -> int:
+        a = []
+        for x in hours:
+            a.append(-1 if x <= 8 else 1)
+
+        ret = 0
+        s = 0
+        deepest = []
+        deepest_index = []
+
+        print(a)
+
+        for i in range(len(a)):
+            s += a[i]
+            if s > 0:
+                ret = i + 1
+            else:
+
+                p = -s
+
+                print(p, i)
+
+                if p < len(deepest_index) and i - deepest_index[p] >ret:
+                    ret = i - deepest_index[p]
+
+                if s < 0 and (len(deepest)==0 or deepest[-1]> s):
+                    deepest.append(s)
+                    deepest_index.append(i)
+
+        print(deepest)
+        print(deepest_index)
+
+        return ret
+
+
 if __name__ == "__main__":
-    hours = [9,9,6,0,6,6,9]
+    hours = [9,9,6,0,6,6,6,6,9,9,9,9,9,9]
     s = Solution()
     print(s.longestWPI(hours))
+    print("------------")
+
+    print(s.longestWPI_DP(hours))
+
 
 
 
